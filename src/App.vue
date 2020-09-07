ce1e098b9fdb79a89524d6be3473a6dff9ba1c68
zhangwei <847521311@qq.com>
2020-09-07 21:59:22 +0800

    tik

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+<div class="mengceng" v-if="isshow">
+  恭喜{{gamer}}获胜
+<button @click="agin()" >重新开始</button>
+</div>
+      <template v-for="i in 9">
+<HelloWorld :key="i" class="item" :show=i :iso=flat @change="change(i,flat)" ref="HWss">
+
+</HelloWorld>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+
+import {Component,Prop,Vue,Ref}from 'vue-property-decorator'
+import HelloWorld from './components/HelloWorld.vue';
+@Component({
+  components:{
+    HelloWorld
+  }
+})
+
+
+export default class App extends Vue{
+public flat:string='o'
+private num:number=0
+private arr:string[]=[];
+private isshow:boolean=false
+private gamer:string|undefined
+/* @Ref('HWss') private hw
+lost(){
+console.log(this.hw);
+
+} */
+agin(){
+  this.isshow=false
+  location.reload()
+}
+private change(i:number,flat:string){
+  console.log(i,flat);
+  this.arr[i-1]=flat
+  this.num=this.num+1
+if(this.num%2==0){
+this.flat='o'
+}else{
+  this.flat='x'
+};
+this.isshow=this.islost(this.arr)
+}
+islost(localarr:string[]):boolean{
+  let flat:boolean=false;
+for(let i=0;i<localarr.length;i++){
+  console.log(localarr[i]);
+  
+  if(i==0 || i==3 || i==5){
+    if(localarr[i]==localarr[i+1]&&localarr[i+1]==localarr[i+2]&&localarr[i]!=undefined){
+      flat=true
+      this.gamer=localarr[i]
+    }}
+    if(i==0 || i==1 || i==2){
+ if(localarr[i]==localarr[i+3]&&localarr[i+3]==localarr[i+6]&&localarr[i]!=undefined){
+      flat=true
+      this.gamer=localarr[i]
+    }
+    }
+     if(i==0){
+       if(localarr[i]==localarr[i+4]&&localarr[i+4]==localarr[i+8]&&localarr[i]!=undefined){
+      flat=true
+      this.gamer=localarr[i]
+    }
+     }
+   if(i==2){
+       if(localarr[i]==localarr[i+2]&&localarr[i+2]==localarr[i+4]&&localarr[i]!=undefined){
+      flat=true
+      this.gamer=localarr[i]
+    }
+     }   
+  
+
+}
+return flat
+}
+}
+</script>
+
+
+
+<style scoped>
+#app{
+  margin-left:550px;
+  margin-top: 200px;
+  width: 650px;
+  height: 650px;
+}
+.item{
+  float: left;
+}
+.mengceng{
+  position: absolute;
+  
+  width: 650px;
+  height: 650px;
+  background-color: rgba(0, 0, 0, .8);
+  color: white;
+  font-size: 100px;
+  text-align: center;
+  line-height: 650px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="Hw">
+    <div class="one" :class="[classstyle,myiso,myisos]" @click="change()" @mouseenter="enter()" @mouseleave="leave()"></div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue, Watch,Emit } from 'vue-property-decorator'
+@Component({})
+export default class HelloWorld extends Vue{
+  private flats:number=0
+  private classstyle:string|undefined
+  private myiso:string|undefined=""
+  private myisos:string|undefined=''
+@Prop(Number) private show:number|undefined
+@Prop(String) private iso:string|undefined
+created(){
+  if(this.show==1 || this.show==2 || this.show==4|| this.show==5){
+this.classstyle="bottom right"
+  }
+  else if(this.show==3|| this.show==6){
+    this.classstyle="bottom"
+  }
+  else if(this.show==7|| this.show==8){
+this.classstyle="right"
+  }else{
+   this.classstyle='' 
+  };
+/*   this.myiso=this.iso */
+/*   this.iso='' */
+};
+@Emit()
+change(){
+this.flats++
+};
+@Watch('iso')
+changiso(newvalue:string,oldvalue:string){  
+if(this.flats!=0){ 
+   if(this.myisos==""){
+  this.myisos=oldvalue+'s'
+}
+this.myiso=newvalue
+}
+} 
+private enter(){
+if(this.myisos==""){
+   this.myiso=this.iso
+}
+
+}
+private leave(){
+if(this.myisos==""){ 
+   this.myiso=""}
+  
+}
+}
+  
+</script>
+
+<style scoped>
+.one{
+  width: 200px;
+  height: 200px;
+}
+.left{
+  border-left: 10px solid black;
+}
+.right{
+  border-right: 10px solid black;
+}
+.top{
+  border-top: 10px solid black;
+}
+.bottom{
+  border-bottom: 10px solid black;
+}
+.o{
+
+}
+.o::after{
+  content: 'o';
+  font-size: 150px;
+  margin-left: 55px;
+  color: cadetblue;
+}
+.x{
+
+}
+.x::after{
+  content: 'x';
+   font-size: 150px;
+  margin-left: 55px;
+  color: cadetblue;
+}
+.os{
+
+}
+.os::after{
+  content: 'o';
+  font-size: 150px;
+  margin-left: 55px;
+  color: black;
+}
+.xs{
+
+}
+.xs::after{
+  content: 'x';
+   font-size: 150px;
+  margin-left: 55px;
+  color: black;
+}
+</style>
